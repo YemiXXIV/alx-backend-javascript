ac9c379acd4a5048accd0369d3fc44127a0c476e
Mustapha <mustapha.yinusa@yahoo.com>
2024-08-02 19:29:30 +0100

    The typescript task

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,18 @@
+
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud';
+
+let row: RowElement = {
+    firstName: 'Guillame',
+    lastName: 'Salva'
+}
+
+const newRowID: RowID = CRUD.insertRow(row);
+
+const updatedRow: RowElement = {
+    ...row,
+    age: 23
+};
+
+CRUD.updaterow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
