9be4f18a3ad7aa1d9240d721b3a22c8c3fead62f
Mustapha <mustapha.yinusa@yahoo.com>
2024-08-05 00:42:41 +0100

    Task 7 fix

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,3 +1,10 @@
-export default function loadBalancer (chinaDownload, USDownload) {
-  Promise.race([chinaDownload, USDownload]).then((value) => console.log(value));
+/** */
+export default function loadBalancer(chinaDownload, USDownload) {
+  return Promise
+    .race([chinaDownload,
+      USDownload])
+    .then((value) => value) // Explicitly return the value
+    .catch((error) => {
+      throw error; // Handle potential rejection
+    });
 }
